e6cab9a18e527f360a36d47754738a50723cb2c7
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-12-04 12:20:00 +0200

    add view for book

---
+++ books/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+
+from books.models import Book
+from books.serializers import BookSerializer
+from books.permissions import IsAdminOrReadOnly
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAdminOrReadOnly,)
